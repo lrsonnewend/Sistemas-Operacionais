58bdb9a8ac3f436d04322f1ccc4784c650327361
lucas <lucas.lr184@gmail.com>
2019-05-04 12:05:33 -0300

    simulador de processos

--- Escalonamento/simulatorProcess.py
+++ Escalonamento/simulatorProcess.py
@@ -17,23 +17,52 @@ def setBurst(burst):
 def setTcheg(tcheg):
     self.tcheg = tcheg
 
+    
+
 infoProcess = []
 
-opcao = input('1 - FCFS\n2 - SJF\n3 - SRTF\n4 - Round Robin\n5 - Multinível\n')
+while(True):
+    opcao = input('1 - FCFS\n2 - SJF\n3 - SRTF\n4 - Round Robin\n5 - Multinível\n6 - Sair\n')
 
-if opcao == '1':
-    qProcess = int(input('Insira a quantidade de processos: '))
+    if opcao == '1':
+        qProcess = int(input('Insira a quantidade de processos: '))
 
-    for i in range(qProcess):
-        b = int(input('Burst: '))
-        c = int(input('Tempo de chegada: '))
-        proc = Processo(i, b, c)
-        infoProcess.append(proc)
+        for i in range(qProcess):
+            print('P '+str(i+1))
+            b = int(input('Burst: '))
+            c = int(input('Tempo de chegada: '))
+            proc = Processo(i, b, c)
+            infoProcess.append(proc)
 
 
-print('\n')
-print('Informações dos processos:\n')
-print('Process',' Burst',' Tempo de chegada')
+        print('\n')
 
-for i in infoProcess:
-    print(i.nome,"      ", i.burst,"      ",i.tcheg)
+        print('Informações dos processos:\n')
+        print('Process',' Burst',' Tempo de chegada')
+
+        count = 0
+        for i in infoProcess:
+            count+=1
+        
+            print(count,"      ", i.burst,"      ",i.tcheg)
+
+        print('\n')
+
+        turnAr = 0
+        count= 0
+
+        for j in infoProcess:
+            count+=1
+            print('Process '+str(count))
+
+            turnAr+= (j.burst - j.tcheg)
+
+            wait = (turnAr - j.burst)
+
+            print('TurnAround Time: '+str(turnAr))
+            print('Waiting Time: '+str(wait))
+            print('\n')
+
+    if opcao == '6':
+        break
+              
